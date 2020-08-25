665ccfee3adba3bfa5e1271fc68993e3d1528774
jdonlucas <jdonlucas93@gmail.com>
2020-08-24 19:22:24 -0500

    Se inicializa proyecto

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 6.48f, -7.7f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Offset the camera behind player's position
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Variables for the speed and direction of the vehicle
+    private float speed = 20.0f;
+    private float turnSpeed = 45;
+    private float horizontalInput;
+    private float forwardInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Getting input from the user
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // Move vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotate vehicle direction
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+        
+    }
+}
